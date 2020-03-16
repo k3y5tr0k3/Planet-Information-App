8c1faeef9328760b3334aff4d02584a7020adcb2
k3y5tr0k3 <bryandonnelly7@gmail.com>
2020-03-16 14:47:33 +0000

    Initial commit

---
+++ src/com/callenge/Main.java
@@ -0,0 +1,14 @@
+
+package com.callenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        // TODO: Configuration
+
+        // TODO: Display Menu
+    }
+}
